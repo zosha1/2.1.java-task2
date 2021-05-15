b690c40657512a0bc8b1b248af80681b208f32cf
Zosha <posyamyka@gmail.com>
2021-05-15 18:39:04 +0300

    bonus

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        double amount = 100;
+        double income = 1100;
+        int coefficient = 100;
+        int limit = 1000;
+        int bonus = 0;
+        if (income >= limit) {
+            bonus = (int) income / coefficient;
+        }
+        amount = amount + income + bonus;
+        System.out.println(amount);
+    }
+}
